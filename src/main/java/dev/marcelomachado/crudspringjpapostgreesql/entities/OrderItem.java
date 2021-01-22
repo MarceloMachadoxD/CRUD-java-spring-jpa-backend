8fb5119fbcfa13e57bb2d2fce679b81aa8fd7a8e
MarceloMachadoxD <mastermanson@gmail.com>
2021-01-22 08:38:39 -0300

    OrderItem - many to many association with extra attributes

---
+++ src/main/java/dev/marcelomachado/crudspringjpapostgreesql/entities/OrderItem.java
@@ -0,0 +1,78 @@
+package dev.marcelomachado.crudspringjpapostgreesql.entities;
+
+import dev.marcelomachado.crudspringjpapostgreesql.entities.PK.OrderItemPK;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    private Integer qty;
+    private Double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Order order, Product product, Integer qty, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.qty = qty;
+        this.price = price;
+    }
+
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
